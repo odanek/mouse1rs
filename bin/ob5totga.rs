04530d2b36274f12ad14b8fae2f5309ff643e5e7
odanek <ondrej.danek@gmail.com>
2022-04-20 23:55:50 +0200

    Ob5 to tga convertor

--- bin/ob5totga.rs
+++ bin/ob5totga.rs
@@ -1,19 +1,19 @@
-use std::{path::Path, fs::File, io::{BufReader, Read}};
+use std::{
+    fs::File,
+    io::{BufReader, BufWriter, Read, Write},
+    path::Path,
+};
 
 #[repr(C)]
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 struct Rgb {
     pub r: u8,
     pub g: u8,
-    pub b: u8
+    pub b: u8,
 }
 
 impl Rgb {
-    const BLACK: Rgb = Rgb {r: 0, g: 0, b: 0};
-
-    pub fn new(r: u8, g: u8, b: u8) -> Self {
-        Self {r, g, b}
-    }
+    const BLACK: Rgb = Rgb { r: 0, g: 0, b: 0 };
 
     pub fn set(&mut self, r: u8, g: u8, b: u8) {
         self.r = r;
@@ -23,11 +23,11 @@ impl Rgb {
 }
 
 struct Palette {
-    data: [Rgb; 256]
+    data: [Rgb; 256],
 }
 
 impl Palette {
-    fn load<P: AsRef<Path>>(path: P) -> std::io::Result<Self> {        
+    fn load<P: AsRef<Path>>(path: P) -> std::io::Result<Self> {
         let file = File::open(path)?;
         let mut reader = BufReader::new(file);
 
@@ -38,14 +38,81 @@ impl Palette {
             color.set(rgb[0], rgb[1], rgb[2]);
         }
 
-        Ok(Self {data})
+        Ok(Self { data })
+    }
+
+    pub fn get(&self, index: usize) -> Rgb {
+        self.data[index]
     }
 }
 
+struct Size {
+    width: u32,
+    height: u32,
+}
 
+impl Size {
+    pub fn new(width: u32, height: u32) -> Self {
+        Self { width, height }
+    }
+}
 
+struct Image {
+    size: Size,
+    data: Vec<Rgb>,
+}
+
+impl Image {
+    pub fn load_ob5<P: AsRef<Path>>(
+        path: P,
+        size: Size,
+        palette: &Palette,
+    ) -> std::io::Result<Self> {
+        let file = File::open(path)?;
+        let mut reader = BufReader::new(file);
+
+        let count = (size.width * size.height) as usize;
+        let mut data = Vec::new();
+        while data.len() < count {
+            let mut input = [0u8; 2];
+            reader.read_exact(&mut input)?;
+            let color = palette.get(input[1] as usize);
+            for _ in 0..input[0] {
+                data.push(color);
+            }
+        }
+
+        Ok(Self { size, data })
+    }
+
+    pub fn save_tga<P: AsRef<Path>>(&self, path: P) -> std::io::Result<()> {
+        let file = File::create(path)?;
+        let mut writer = BufWriter::new(file);
+
+        let wb = self.size.width.to_le_bytes();
+        let hb = self.size.height.to_le_bytes();
+        let header: [u8; 18] = [
+            0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, wb[0], wb[1], hb[0], hb[1], 24, 0,
+        ];
+        writer.write_all(&header)?;
+
+        for row in self.data.chunks(self.size.width as usize).rev() {
+            for rgb in row {
+                let color = [rgb.b, rgb.g, rgb.r];
+                writer.write_all(&color)?;
+            }
+        }
+
+        Ok(())
+    }
+}
 
 fn main() {
-    let palette = Palette::load("assets/vga.pal").unwrap();
-    println!("OK"); //, std::env::current_dir().unwrap());
+    let palette = Palette::load("assets/vga.pal").expect("Unable to load palette");
+    let image =
+        Image::load_ob5("lev1.kr3", Size::new(320, 192), &palette).expect("Unable to load image");
+    image
+        .save_tga("lev1.tga")
+        .expect("Unable to write output file");
+    println!("Done");
 }
