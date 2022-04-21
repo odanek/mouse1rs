01a0e5ccd3fcd1e1a98ee3358a5c97f60415dfe7
odanek <ondrej.danek@gmail.com>
2022-04-21 21:35:05 +0200

    Image joining

--- src/bin/ob5totga.rs
+++ src/bin/ob5totga.rs
@@ -46,6 +46,7 @@ impl Palette {
     }
 }
 
+#[derive(Clone, Copy, Debug)]
 struct Size {
     width: u32,
     height: u32,
@@ -54,6 +55,10 @@ struct Size {
 impl Size {
     pub fn new(width: u32, height: u32) -> Self {
         Self { width, height }
+    }
+
+    pub fn count(&self) -> usize {
+        (self.width * self.height) as usize
     }
 }
 
@@ -66,23 +71,29 @@ impl Image {
     pub fn load_ob5<P: AsRef<Path>>(
         path: P,
         size: Size,
+        image_count: usize,
         palette: &Palette,
-    ) -> std::io::Result<Self> {
+    ) -> std::io::Result<Vec<Self>> {
         let file = File::open(path)?;
         let mut reader = BufReader::new(file);
 
-        let count = (size.width * size.height) as usize;
-        let mut data = Vec::new();
-        while data.len() < count {
-            let mut input = [0u8; 2];
-            reader.read_exact(&mut input)?;
-            let color = palette.get(input[1] as usize);
-            for _ in 0..input[0] {
-                data.push(color);
+        let mut result = Vec::new();
+        let count = size.count();
+
+        for _ in 0..image_count {
+            let mut data = Vec::new();
+            while data.len() < count {
+                let mut input = [0u8; 2];
+                reader.read_exact(&mut input)?;
+                let color = palette.get(input[1] as usize);
+                for _ in 0..input[0] {
+                    data.push(color);
+                }
             }
+            result.push(Self { size, data });
         }
 
-        Ok(Self { size, data })
+        Ok(result)
     }
 
     pub fn save_tga<P: AsRef<Path>>(&self, path: P) -> std::io::Result<()> {
@@ -105,13 +116,36 @@ impl Image {
 
         Ok(())
     }
+
+    pub fn join_horizontal(images: &[Image]) -> Image {
+        let width = images
+            .iter()
+            .fold(0u32, |width, image| width + image.size.width);
+        let size = Size::new(width, images[0].size.height);
+        let usize_total_width = size.width as usize;
+        let mut data = vec![Rgb::BLACK; size.count()];
+
+        let mut left = usize_total_width;
+        for image in images {
+            let usize_image_width = image.size.width as usize;
+            left -= usize_image_width;
+            let mut dest = left;
+            for row in image.data.chunks(usize_image_width) {
+                data.splice(dest..(dest + usize_image_width), row.iter().copied());
+                dest += usize_total_width;
+            }
+        }
+
+        Image { size, data }
+    }
 }
 
 fn main() {
     let palette = Palette::load("assets/vga.pal").expect("Unable to load palette");
-    let image =
-        Image::load_ob5("lev1.kr3", Size::new(320, 192), &palette).expect("Unable to load image");
-    image
+    let images = Image::load_ob5("lev1.kr3", Size::new(320, 192), 11, &palette)
+        .expect("Unable to load image");
+    let joined = Image::join_horizontal(&images[0..10]);
+    joined
         .save_tga("lev1.tga")
         .expect("Unable to write output file");
     println!("Done");
