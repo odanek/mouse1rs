43290607c42507645d381d98f53964ee66064dbb
odanek <ondrej.danek@gmail.com>
2025-11-02 21:39:20 +0100

    Fix Rust 1.91 warning

--- src/hit_map.rs
+++ src/hit_map.rs
@@ -92,7 +92,7 @@ impl AssetLoader for HitMapLoader {
         &self,
         bytes: &[u8],
         load_context: &mut LoadContext,
-    ) -> BoxedFuture<anyhow::Result<()>> {
+    ) -> BoxedFuture<'_, anyhow::Result<()>> {
         let hit_map = HitMap::from_rle_bytes(bytes);
         load_context.set_default_asset(LoadedAsset::new(hit_map));
         Box::pin(async move { Ok(()) })
